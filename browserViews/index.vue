3a67bf09d3ff4db58986d872f8bb872ab138faab
Zatsune No Mokou <contact@zatsunenomokou.eu>
2019-05-27 15:20:14 +0200

    more grid in main frame

--- browserViews/index.vue
+++ browserViews/index.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
-	main
-		p
+	main.grid.no-c-gap
+		p.grid-12
 			button(v-on:click='onStreamLink') Ouvrir streamlink
 			button(v-on:click='reloadIframe') Run code !
 
-		p
+		p.grid-12
 			// All of the Node.js APIs are available in this renderer process.
 			| We are using Node.js {{versions.node}}, Chromium {{versions.chrome}}, and Electron {{versions.electron}}.
 
-		div#codeInputs.grid.no-c-gap
-			div#input1.grid-4
-			div#input2.grid-4
-			div#input3.grid-4
-		iframe#iframe(ref='iframe', sandbox='allow-same-origin allow-scripts', src='iframe.html')
+		div#input1.grid-4
+		div#input2.grid-4
+		div#input3.grid-4
+
+		iframe#iframe.grid-12(ref='iframe', sandbox='allow-same-origin allow-scripts', src='iframe.html')
 </template>
 
 <script>
