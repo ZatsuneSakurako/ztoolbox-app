87e5601fe7e3a0aae31349b3f50dc5266af20139
Zatsune No Mokou <contact@zatsunenomokou.eu>
2019-05-27 21:12:17 +0200

    move code of main page to its vue file

--- browserViews/index.vue
+++ browserViews/index.vue
@@ -1,23 +1,152 @@
 <template lang="pug">
-	main.grid.no-c-gap
+	main.grid.no-c-gap.no-r-gap
 		p.grid-12
+			input#streamlink(type='radio', v-model='menu', value='streamlink')
+			label(for='streamlink') Streamlink
+			input#codeTester(type='radio', v-model='menu', value='code-tester')
+			label(for='codeTester') Code tester
+
+		p.grid-12(v-if='menu === \'streamlink\'')
 			button(v-on:click='onStreamLink') Ouvrir streamlink
+
+		p.grid-12(v-if='menu === \'code-tester\'')
 			button(v-on:click='reloadIframe') Run code !
 
-		p.grid-12
+		p.grid-12(v-if='menu === \'streamlink\'')
 			// All of the Node.js APIs are available in this renderer process.
 			| We are using Node.js {{versions.node}}, Chromium {{versions.chrome}}, and Electron {{versions.electron}}.
 
-		div#input1.grid-4
-		div#input2.grid-4
-		div#input3.grid-4
+		div#input1.grid-4(ref='input1', v-if='menu === \'code-tester\'')
+		div#input2.grid-4(ref='input2', v-if='menu === \'code-tester\'')
+		div#input3.grid-4(ref='input3', v-if='menu === \'code-tester\'')
 
-		iframe#iframe.grid-12(ref='iframe', sandbox='allow-same-origin allow-scripts', src='iframe.html')
+		iframe#iframe.grid-12(ref='iframe', sandbox='allow-same-origin allow-scripts', src='iframe.html', v-if='menu === \'code-tester\'')
 </template>
 
 <script>
+	const { ipcRenderer } = require('electron'),
+		editors = {
+			html: '<h3>No need to write &lt;body&gt; &lt;/body&gt;</h3>',
+			css: 'body {\n\tpadding: 0;\n}',
+			js: 'function test(){return true;}'
+		}
+	;
+
+
+
+
+
+	function codeTesterLoader() {
+		const defaultOptions = {
+			indentWithTabs: true,
+			lineNumbers: true,
+			lineSeparator: "\n",
+			lineWrapping: true,
+			theme: 'monokai'
+		};
+
+		const htmlEditor = new CodeMirror(this.$refs.input1, Object.assign({
+			value: editors.html,
+			mode: 'htmlmixed'
+		}, defaultOptions));
+		const cssEditor = new CodeMirror(this.$refs.input2, Object.assign({
+			value: editors.css,
+			mode: 'css'
+		}, defaultOptions));
+		const jsEditor = new CodeMirror(this.$refs.input3, Object.assign({
+			value: editors.js,
+			mode: 'javascript'
+		}, defaultOptions));
+
+
+
+		/**
+		 *
+		 * @type {HTMLIFrameElement}
+		 */
+		const iframe = this.$refs.iframe;
+		iframe.addEventListener('load', function () {
+			const {VM} = require('vm2'),
+				stripHtml = require('string-strip-html')
+			;
+
+			const iframeWin = iframe.contentWindow;
+			// @ts-ignore
+			iframeWin.stripHtml = stripHtml;
+			// @ts-ignore
+			iframeWin.VM = VM;
+
+			iframeWin.postMessage({
+				type: 'init'
+			}, location.href);
+
+
+
+			try {
+				editors.html = htmlEditor.getValue();
+				iframeWin.postMessage({
+					type: 'html',
+					html: editors.html
+				}, location.href);
+			} catch (e) {
+				console.error(e);
+			}
+
+			try {
+				editors.css = cssEditor.getValue();
+				iframeWin.postMessage({
+					type: 'css',
+					css: editors.css
+				}, location.href);
+			} catch (e) {
+				console.error(e);
+			}
+
+			try {
+				editors.js = jsEditor.getValue();
+				iframeWin.postMessage({
+					type: 'js',
+					js: editors.js
+				}, location.href);
+			} catch (e) {
+				console.error(e);
+			}
+		});
+
+
+
+		iframe.contentWindow.location.reload();
+	}
+
+
+
+
+
 	export default {
-		name: "index"
+		name: "index",
+		methods: {
+			nextTick() {
+				return this.constructor.nextTick()
+			},
+			onStreamLink: function () {
+				ipcRenderer.send('openStreamlink');
+			},
+			reloadIframe: function () {
+				this.$refs.iframe.contentWindow.location.reload();
+			}
+		},
+		watch: {
+			menu: function (val) {
+				if (val === 'code-tester') {
+					this.nextTick()
+						.then(() => {
+							codeTesterLoader.call(this);
+						})
+						.catch(console.error)
+					;
+				}
+			}
+		}
 	}
 </script>
 
--- browserViews/js/index.ts
+++ browserViews/js/index.ts
@@ -1,4 +1,3 @@
-declare var CodeMirror : any;
 interface CompiledVue {
 	render: Function,
 	staticRenderFns: Function[]
@@ -6,12 +5,9 @@ interface CompiledVue {
 
 
 
-const path = require('path'),
-	Vue = require('vue'),
-	{ ipcRenderer } = require('electron')
-;
 
 const data = {
+	menu: 'streamlink',
 	message: 'Hello Vue!',
 	versions: process.versions
 };
@@ -19,95 +15,11 @@ const data = {
 
 
 window.addEventListener("load", function () {
-	const indexTemplate:CompiledVue = require(path.resolve(__dirname, './index.js'));
+	const indexTemplate:CompiledVue = require(__dirname + '\\' + 'index.js');
+	const Vue = require('vue');
 
 	const app = new Vue(Object.assign({
 		el: 'main',
-		data: data,
-		methods: {
-			onStreamLink: function () {
-				ipcRenderer.send('openStreamlink');
-			},
-			reloadIframe: function () {
-				app.$refs.iframe.contentWindow.location.reload();
-			}
-		}
+		data: data
 	}, indexTemplate));
-
-
-
-
-
-	const defaultOptions = {
-		indentWithTabs: true,
-		lineNumbers: true,
-		lineSeparator: "\n",
-		lineWrapping: true,
-		theme: 'monokai'
-	};
-
-	const htmlEditor = new CodeMirror(document.querySelector('#input1'), Object.assign({
-		value: '<h3>No need to write &lt;body&gt; &lt;/body&gt;</h3>',
-		mode: 'htmlmixed'
-	}, defaultOptions));
-	const cssEditor = new CodeMirror(document.querySelector('#input2'), Object.assign({
-		value: "body {\n\tpadding: 0;\n}",
-		mode: 'css'
-	}, defaultOptions));
-	const jsEditor = new CodeMirror(document.querySelector('#input3'), Object.assign({
-		value: 'function test(){return true;}',
-		mode: 'javascript'
-	}, defaultOptions));
-
-
-
-	const iframe = document.querySelector('#iframe') as HTMLIFrameElement;
-	iframe.addEventListener('load', function () {
-		const {VM} = require('vm2'),
-			stripHtml = require('string-strip-html')
-		;
-
-		const iframeWin = iframe.contentWindow;
-		// @ts-ignore
-		iframeWin.stripHtml = stripHtml;
-		// @ts-ignore
-		iframeWin.VM = VM;
-
-		iframeWin.postMessage({
-			type: 'init'
-		}, location.href);
-
-
-
-		try {
-			iframeWin.postMessage({
-				type: 'html',
-				html: htmlEditor.getValue()
-			}, location.href);
-		} catch (e) {
-			console.error(e);
-		}
-
-		try {
-			iframeWin.postMessage({
-				type: 'css',
-				css: cssEditor.getValue()
-			}, location.href);
-		} catch (e) {
-			console.error(e);
-		}
-
-		try {
-			iframeWin.postMessage({
-				type: 'js',
-				js: jsEditor.getValue()
-			}, location.href);
-		} catch (e) {
-			console.error(e);
-		}
-	});
-
-
-
-	iframe.contentWindow.location.reload();
 });
