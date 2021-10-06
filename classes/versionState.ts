405b002bc67969b26a6170a618bbd540b9ce77c4
Zatsune No Mokou <contact@zatsunenomokou.eu>
2021-10-06 23:53:19 +0200

    versionState get gitlab repo id

--- classes/versionState.ts
+++ classes/versionState.ts
@@ -11,17 +11,28 @@ export async function versionState(gitDir=process.cwd()) {
 
 	const dirGit = simpleGit({baseDir: gitDir}),
 		status = await dirGit.status(),
-		logs = (await dirGit.log()).latest
+		logs = (await dirGit.log()).latest,
+		[firstRemote] = (await dirGit.getRemotes(true))
 	;
 
 	if (!logs || !status.current) {
 		throw new Error('LATEST_COMMIT_ERROR');
 	}
 
+	const remoteOrigin = firstRemote.refs.fetch,
+		originTest = /(?:git@gitlab\.com:|https:\/\/gitlab\.com\/)([^\/]+\/[^\/]+)\.git/i,
+		result = originTest.exec(remoteOrigin),
+		remoteGitlabOrigin = result ? result[1] : null
+	;
+
 	return <VersionState>{
 		branch: status.current, // current branch
 		commitHash: logs.hash,
-		commitDate: new Date(logs.date) // current last local commit date
+		commitDate: new Date(logs.date), // current last local commit date
+		remoteOrigin: {
+			ref: remoteOrigin,
+			gitLabPath: remoteGitlabOrigin
+		}
 	}
 }
 
