712ce214e2122f01206f8330c67b724dc111625b
Zatsune No Mokou <contact@zatsunenomokou.eu>
2022-04-04 21:56:57 +0200

    update gitignore + yarn 2 + old notify

--- classes/notify.ts
+++ classes/notify.ts
@@ -1,50 +1,6 @@
 import {Notification} from "electron";
-import notifier, {NotificationMetadata, NotificationCallback} from "node-notifier";
-import NotifySend from "node-notifier/notifiers/notifysend";
-import WindowsToaster from "node-notifier/notifiers/toaster";
-import NotificationCenter from "node-notifier/notifiers/notificationcenter";
 import {appIconPath_x3} from "./constants";
 import {NotificationResponse} from "./bo/notify";
-
-
-
-export function notify(options:{id?: number, title:string, message:string, icon?:string, remove?:number, sound?:boolean}):Promise<NotificationResponse & {metadata?: NotificationMetadata}> {
-	return new Promise((resolve, reject) => {
-		if (options === null || typeof options !== 'object') {
-			reject('WrongArgument');
-			return;
-		}
-
-		let opts : notifier.Notification | NotifySend.Notification | WindowsToaster.Notification | NotificationCenter.Notification = <notifier.Notification>{
-			id: options.id,
-			title: options.title,
-			message: options.message,
-			icon: appIconPath_x3,
-			remove: options.remove,
-			wait: true,
-		};
-		if (process.platform === "win32") {
-			const _opts = <WindowsToaster.Notification>opts;
-			_opts.sound = options.sound ?? true;
-		} else if (process.platform === "linux") {
-			const _opts = <NotifySend.Notification>opts;
-		} else if (process.platform === 'darwin') {
-			const _opts = <NotificationCenter.Notification>opts;
-			_opts.sound = options.sound ?? true;
-		}
-
-		notifier.notify(opts, <NotificationCallback>function (error:Error | null, response:string, metadata?: NotificationMetadata) {
-			if (!!error) {
-				reject(error);
-			} else if (!(typeof <any>response === 'string' && response.indexOf('clicked'))) {
-				resolve({
-					response,
-					metadata
-				});
-			}
-		});
-	})
-}
 
 
 
