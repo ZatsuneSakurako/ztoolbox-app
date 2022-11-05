a44fcd27254ac9913d85954d157c3816214cb82c
Zatsune No Mokou <contact@zatsunenomokou.eu>
2022-11-05 14:26:15 +0100

    chrome-native - interface syntax

--- classes/bo/chromeNative.ts
+++ classes/bo/chromeNative.ts
@@ -6,18 +6,18 @@ export type SocketMessage<T> = {error: false} & {result: T} | {error: true|strin
 export type ResponseCallback<T> = (response:SocketMessage<T>) => void;
 
 export interface ServerToClientEvents {
-	'ws open': (data: SocketMessage<{ connected: string }>) => void;
-	log: (...data: any[]) => void;
-	ping: (cb: ResponseCallback<'pong'>) => void;
+	'ws open'(data: SocketMessage<{ connected: string }>): void
+	log(...data: any[]): void
+	ping(cb: ResponseCallback<'pong'>): void
 }
 
 export interface ClientToServerEvents {
-	getPreference: (id: string, cb: ResponseCallback<{ id: string, value: undefined|RandomJsonData }>) => void;
-	getPreferences: (ids: string[], cb: ResponseCallback<{ id: string, value: undefined|RandomJsonData }[]>) => void;
-	getDefaultValues: (cb: ResponseCallback<SettingsConfig>) => void;
-	ping: (cb: ResponseCallback<'pong'>) => void;
-	showSection: (sectionName: string, cb: ResponseCallback<'success'>) => void;
-	extensionName: (extensionName: IChromeExtensionName) => void;
+	getPreference(id: string, cb: ResponseCallback<{ id: string, value: undefined|RandomJsonData }>): void
+	getPreferences(ids: string[], cb: ResponseCallback<{ id: string, value: undefined|RandomJsonData }[]>): void
+	getDefaultValues(cb: ResponseCallback<SettingsConfig>): void
+	ping(cb: ResponseCallback<'pong'>): void
+	showSection(sectionName: string, cb: ResponseCallback<'success'>): void
+	extensionName(extensionName: IChromeExtensionName): void
 	getWebsitesData(cb: ResponseCallback<Dict<IJsonWebsiteData>>): void
 	sendWebsitesData(websiteData: Dict<IJsonWebsiteData>): void
 }
