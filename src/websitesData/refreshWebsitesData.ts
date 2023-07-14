61dabba40a12387228d777cc98f43ffe3dbfbe45
Zatsune No Mokou <ZatsuneNoMokou@users.noreply.github.com>
2023-07-14 10:03:29 +0200

    refactor websites data

--- src/websitesData/refreshWebsitesData.ts
+++ src/websitesData/refreshWebsitesData.ts
@@ -1,79 +1,14 @@
-import {IJsonWebsiteData} from "../browserViews/js/bo/websiteData";
-import {ZAlarm} from "./ZAlarm";
-import {settings} from "../main";
-import electron, {BrowserWindow, ipcMain} from "electron";
-import {websitesData, websitesDataLastRefresh} from "./Settings";
-import {JsonSerialize} from "./JsonSerialize";
-import {WebsiteData} from "../browserViews/js/websiteData";
-import {setBadge} from "./windowManager";
-import {io, remoteSocket, sendNotification} from "./chromeNative";
-import {Dict} from "../browserViews/js/bo/Dict";
-import {i18n} from "../src/i18next";
-
-async function doNotifyWebsite(website:string, websiteData:WebsiteData) {
-	const i18ex = await i18n;
-
-	const labelArray : string[] = [];
-	if (websiteData.logged && websiteData.hasOwnProperty('folders')) {
-		for (let [name, folderData] of websiteData.folders) {
-			let count = folderData.folderCount;
-			if (!isNaN(count) && count > 0) {
-				let suffix = '';
-				if (websiteData.notificationState.count !== null && websiteData.count > websiteData.notificationState.count) {
-					suffix = ` (+${websiteData.count - websiteData.notificationState.count})`;
-				}
-				labelArray.push(`${name}: ${count}${suffix}`);
-			}
-		}
-	}
-
-	if (!websiteData.logged) {
-		const oldLoggedState = websiteData.notificationState.logged;
-		if (oldLoggedState || oldLoggedState === undefined) {
-			sendNotification({
-				'title': i18ex('websitesData.website_notif', {'website': website}),
-				'message': i18ex('websitesData.website_not_logged', {'website': website})
-			})
-				.then(notificationResponse => {
-					if (notificationResponse?.response === 'click' && websiteData.href) {
-						electron.shell.openExternal(websiteData.href);
-					}
-				})
-				.catch(console.error)
-			;
-		}
-		websiteData.notificationState.logged = !!websiteData.logged;
-	} else if (websiteData.notificationState.count === null || websiteData.count > websiteData.notificationState.count) {
-		if (settings.getBoolean('notify_checkedData')) {
-			sendNotification({
-				"title": i18ex('websitesData.website_notif', {'website': website}),
-				"message": i18ex('websitesData.count_new_notif', {'count': websiteData.count}) + "\n" + labelArray.join("\n"),
-			})
-				.then(notificationResponse => {
-					if (notificationResponse?.response === 'click' && websiteData.href) {
-						electron.shell.openExternal(websiteData.href);
-					}
-				})
-				.catch(console.error)
-			;
-		}
-	} else if (settings.getBoolean('notify_all_viewed') && websiteData.count === 0 && websiteData.notificationState.count > 0) {
-		sendNotification({
-			"title": i18ex('websitesData.website_notif', {'website': website}),
-			"message": i18ex('websitesData.all_viewed')
-		})
-			.then(notificationResponse => {
-				if (notificationResponse?.response === 'click' && websiteData.href) {
-					electron.shell.openExternal(websiteData.href);
-				}
-			})
-			.catch(console.warn)
-		;
-	}
-
-	websiteData.notificationState.count = websiteData.count;
-}
-
+import {IJsonWebsiteData} from "../../browserViews/js/bo/websiteData";
+import {ZAlarm} from "../../classes/ZAlarm";
+import {settings} from "../../main";
+import {BrowserWindow, ipcMain} from "electron";
+import {websitesData, websitesDataLastRefresh} from "../../classes/Settings";
+import {JsonSerialize} from "../../classes/JsonSerialize";
+import {WebsiteData} from "../../browserViews/js/websiteData";
+import {setBadge} from "../../classes/windowManager";
+import {io, remoteSocket} from "../../classes/chromeNative";
+import {Dict} from "../../browserViews/js/bo/Dict";
+import {doNotifyWebsite} from "./doNotifyWebsite";
 
 
 function _getWebsitesData(socket: remoteSocket): Promise<Dict<IJsonWebsiteData>> {
