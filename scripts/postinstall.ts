f10671737c0c51f701bec7aaa9bae21dc32a19ad
Zatsune No Mokou <ZatsuneNoMokou@users.noreply.github.com>
2025-06-01 00:26:00 +0200

    mv to material symbol

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -2,11 +2,9 @@
 
 import fs from "fs-extra";
 import path from "path";
-import browserify from "browserify";
-import {fileURLToPath} from "url";
+import * as sass from "sass";
 
-const __filename = fileURLToPath(import.meta.url),
-	currentPath = path.resolve(path.dirname(__filename), '..'),
+const currentPath = path.resolve(import.meta.dirname, '..'),
 	libPath = path.resolve(currentPath, './browserViews/lib/')
 ;
 
@@ -19,14 +17,26 @@ if (fs.existsSync(vuePath)) {
 	fs.removeSync(vuePath);
 }
 
-fs.copy(path.resolve(currentPath, './node_modules/@fontsource/ubuntu/'), path.resolve(libPath, './fontsource-ubuntu'))
+await fs.copy(path.resolve(currentPath, './node_modules/@fontsource/ubuntu/'), path.resolve(libPath, './fontsource-ubuntu'))
 	.catch(console.error)
 ;
 
-fs.copy(path.resolve(currentPath, './node_modules/yaml/browser/dist/'), path.resolve(libPath, './yaml'))
+await fs.copy(path.resolve(currentPath, './node_modules/yaml/browser/dist/'), path.resolve(libPath, './yaml'))
 	.catch(console.error)
 ;
 
-fs.copy(path.resolve(currentPath, './node_modules/flems/dist/'), path.resolve(libPath, './flems'))
+await fs.copy(path.resolve(currentPath, './node_modules/flems/dist/'), path.resolve(libPath, './flems'))
 	.catch(console.error)
 ;
+
+
+await fs.mkdirp(path.resolve(libPath, './material-symbols'));
+await fs.copy("./node_modules/material-symbols/material-symbols-outlined.woff2", path.resolve(libPath, './material-symbols/material-symbols-outlined.woff2'));
+await fs.copy("./node_modules/material-symbols/material-symbols-rounded.woff2", path.resolve(libPath, './material-symbols/material-symbols-rounded.woff2'));
+await fs.copy("./node_modules/material-symbols/material-symbols-sharp.woff2", path.resolve(libPath, './material-symbols/material-symbols-sharp.woff2'));
+fs.writeFileSync(path.resolve(libPath, './material-symbols/material-symbols.css'),
+	sass.compile("./node_modules/material-symbols/index.scss").css
+		.replace(/ {2}/g, '\t')
+		.replace(/(font-family: "Material Symbols \w+?";)/g, '/*noinspection CssNoGenericFontName*/\n\t$1 /* stylelint-disable-line font-family-no-missing-generic-family-keyword */'),
+	{ encoding: 'utf-8' }
+);
